878fcc17d92001e4b916d85f11f3af6ccd54813e
Kirill Zolotarev <macbook@MacBook-Pro-macbook.local>
2024-05-20 18:04:33 +0300

    update tests

--- tests/test_date_handler.py
+++ tests/test_date_handler.py
@@ -1,7 +1,8 @@
 import pytest
 from src.date_handler import compare_dates
 
-def test_compare_dates_exact_now(monkeypatch):
+
+def test_compare_dates_exact_now():
     dt2 = 'Пн 20.05 15:00'
     dt1 = 'Вт 28.05 15:00'
     assert not compare_dates(dt2)
